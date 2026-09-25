7ab97c0938cb2e7df39f8b6a3e253a5b9898be3a
tecnologia-Bogota <tecnobogd1s@gmail.com>
2026-09-25 09:56:41 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Cotizador Seguridad PRO · service worker: funciona sin internet y se actualiza solo.
-const CACHE = "cotizador-pro-v6";
+const CACHE = "cotizador-pro-v7";
 const SHELL = ["./", "index.html", "manifest.webmanifest", "icon-192.png", "icon-512.png"];
 self.addEventListener("install", e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL))); self.skipWaiting(); });
 self.addEventListener("activate", e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))); self.clients.claim(); });
